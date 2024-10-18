6f2850ea3c62c7d341ee14068fb9e64a3cbf7012
Toluwaloju0 <teecoded1999@gmail.com>
2024-10-18 13:12:05 +0100

    sql

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,6 @@
+-- A script to create a table with unique characters
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARC(255)
+);
