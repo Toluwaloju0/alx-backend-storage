15f5892686a4f63d21811da53c6a8c85a07083e2
Toluwaloju0 <teecoded1999@gmail.com>
2024-10-23 09:16:52 +0100

    no SQL

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""A script to show log files"""
+
+if __name__ == '__main__':
+    from pymongo import MongoClient
+
+    client = MongoClient()
+    nginx_col = client.logs.nginx
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+
+    print(f"{nginx_col.count_documents({})} logs\nMethods:")
+    for method in methods:
+        print(f"\tmethod {method}:\
+{nginx_col.count_documents({'method': method})}")
+    print(f"{nginx_col.count_documents({'path': '/status'})} status check")
