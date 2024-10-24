19e2ba0530738870dec2aada3ee3089e5ece1c65
Toluwaloju0 <teecoded1999@gmail.com>
2024-10-24 04:35:20 +0100

    redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,8 +6,21 @@ import redis
 from typing import Any, Callable
 
 
-# def count_calls(method: Callable) -> Callable:
-#     @wraps(method)
+method_count = {}
+
+
+def count_calls(method: Callable) -> Callable:
+    @wraps(method)
+    def wrapper(*args, **kwargs):
+        """A function to process a method and return it"""
+        if method_count.get(method.__qualname__):
+            method_count[method.__qualname__] += 1
+        else:
+            method_count[method.__qualname__] = 1
+        result = method(args[0], args[1], key=method.__qualname__)
+        return result
+    return wrapper
+
 
 class Cache:
     """A class for the redis database"""
@@ -17,16 +30,20 @@ class Cache:
 
         self._redis.flushdb()
 
-    def store(self, data: Any) -> str:
+    @count_calls
+    def store(self, data: Any, key=None) -> str:
         """A function to store into a redis database"""
 
-        import uuid
+        if not key:
+            import uuid
 
-        id = str(uuid.uuid4())
+            id = str(uuid.uuid4())
 
-        self._redis.set(id, data)
+            self._redis.set(id, data)
 
-        return id
+            return id
+        else:
+            self._redis.set(key, data)
 
     def get(self, key: str, fn: Callable = None):
         """A function to get and convert data from redis"""
