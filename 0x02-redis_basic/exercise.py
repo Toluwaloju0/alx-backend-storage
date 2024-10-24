64e145cd00985516d3fa47dbcb92ccd54dc98df0
Toluwaloju0 <teecoded1999@gmail.com>
2024-10-24 22:58:13 +0100

    redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -14,7 +14,7 @@ def count_calls(method: Callable) -> Callable:
         result = method(*args)
         self = args[0]
         key = method.__qualname__
-        self._redis.incr(key))
+        self._redis.incr(key)
         return result
     return wrapper
 
